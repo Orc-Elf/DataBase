a4b36c5d8a112419708bfb92da9328452308eb46
Li Jiaxing <li13835183722@outlook.com>
2023-11-21 21:29:45 +0800

    commit

--- Orc/NetMusicShop/MusicShop.sql
+++ Orc/NetMusicShop/MusicShop.sql
@@ -2,3 +2,12 @@ select UserName, count(SongID) as 收藏数
 from collections
 group by UserName
 HAVING 收藏数 >= 2;
+
+
+select AlbumName, count(SongNumber) as 歌曲数
+from albums, songs
+where albums.AlbumID = songs.AlbumID
+group by  AlbumName
+HAVING 歌曲数 >=10;
+
+
